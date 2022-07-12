fa888709af3ac2e7f45dd4fc29979d82f6455b00
lixingjun <lixingjun869@163.com>
2022-07-12 12:31:19 +0800

    add closure example

---
+++ wasm/04.closure/pkg.js
@@ -0,0 +1,14 @@
+
+
+
+mergeInto(LibraryManager.library, {
+    js_add: function (a, b) {
+        console.log("js_add");
+        return jsShowMeTheAnswer(a, b);
+    },
+
+    js_console_log_int: function (param) {
+        console.log("js_console_log_int:" + param);
+    }
+})
+
